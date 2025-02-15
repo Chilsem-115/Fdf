6dd542b01285c9df212a2925dbf6d77dd87d1f9f
Chilsem <tamsamani.ibrahim115@gmail.com>
2025-02-15 22:35:04 +0100

    Create ft_atoi.c

---
+++ lib/libft/ft_atoi.c
@@ -0,0 +1,23 @@
+int	ft_atoi(char *str)
+{
+	int	nb;
+	int	sign;
+
+	nb = 0;
+	sign = 1;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		nb = (nb * 10) + (*str - '0');
+		str++;
+	}
+	return (nb * sign);
+}
